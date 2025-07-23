57d3260f1241632fbd97308841c4c3c73bbf00b8
Toby Armstrong <513578967@qq.com>
2025-07-23 20:25:00 +0800

    初始化

--- src/main/java/com/puzzle/ui/app.java
+++ src/main/java/com/puzzle/ui/app.java
@@ -1,7 +1,12 @@
 package com.puzzle.ui;
 
+import javax.swing.*;
+
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame game = new JFrame("Puzzle");
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 }
