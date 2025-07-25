aa671dd7925b132afe11fbe158945dab96402621
Toby Armstrong <513578967@qq.com>
2025-07-25 11:54:57 +0800

    UI

--- src/main/java/APP.java
+++ src/main/java/APP.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class APP {
     public static void main(String[] args) {
         new GameFrame();
-//        new RegisterFrame();
-//        new LoginFrame();
+        new RegisterFrame();
+        new LoginFrame();
     }
 }
